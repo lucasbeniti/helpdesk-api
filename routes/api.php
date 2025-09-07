a053dfee188b4f976edb48a01d8b0f3019f8c564
Lucas André Beniti Bernardo <lucasbeniti.bernardo@gmail.com>
2025-09-06 21:41:54 -0300

    feat: add Comment routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\TicketController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,5 @@ Route::prefix('/auth')->controller(AuthController::class)->group(function () {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/tickets', TicketController::class);
+    Route::apiResource('/comments', CommentController::class);
 });
